30f1b30fe58004ed36443c9a172b85a09920c764
Ron Severdia <severdia@gmail.com>
2016-06-13 09:59:22 -0700

    Language updates, other cleanup

--- plg_js_completeprofile/js_completeprofile.php
+++ plg_js_completeprofile/js_completeprofile.php
@@ -1,11 +1,6 @@
 <?php
 /**
-* @copyright (C) 2013 iJoomla, Inc. - All rights reserved.
 * @license GNU General Public License, version 2 (http://www.gnu.org/licenses/gpl-2.0.html)
-* @author iJoomla.com <webmaster@ijoomla.com>
-* @url https://www.jomsocial.com/license-agreement
-* The PHP code portions are distributed under the GPL license. If not otherwise stated, all images, manuals, cascading style sheets, and included JavaScript *are NOT GPL, and are released under the IJOOMLA Proprietary Use License v1.0
-* More info at https://www.jomsocial.com/license-agreement
 */
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
@@ -44,7 +39,7 @@ class plgSystemForceFields extends JPlugin
 		}
 		
 		//Load the language file - we might dont need this anymore for Joomla 3.1
-		JPlugin::loadLanguage( 'plg_system_forcefields', JPATH_ADMINISTRATOR );
+		JPlugin::loadLanguage( 'plg_js_completeprofile', JPATH_ADMINISTRATOR );
 		
 		// Let Alex figure this out
 		// set the jinput
@@ -84,7 +79,7 @@ class plgSystemForceFields extends JPlugin
 					if ($field['required']){
 						if( !CProfileLibrary::validateField( $field['id'], $field['type'] , $field['value'] , $field['required']) ){
 							$url = 'index.php?option=com_community&view=profile&task=edit';
-							$message = JText::_('PLG_FORCEFIELDS_MSG');
+							$message = JText::_('PLG_JSCOMPLETEPROFILE_MSG');
 							$mainframe->enqueueMessage( CTemplate::quote($message) , 'error' );
 							$mainframe->redirect($url);
 						}
